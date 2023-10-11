75e6da8894fd593b7cad73af3f3e4932a98f24f7
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-11 12:37:25 +0530

    Update Hello World.py

--- Easy Level Programs/Topic_Programs/Hello World.py
+++ Easy Level Programs/Topic_Programs/Hello World.py
@@ -1,9 +1,9 @@
-//Approach 1 - Just Print
+#Approach 1 - Just Print
 def helloWorld(name):
     print("Hello"+" "+name)
 
 
-//Approach 2 - Store in Variables and Print the variable
+#Approach 2 - Store in Variables and Print the variable
 def helloWorld(name):
     nam = "Hello"
     print(nam+name)
