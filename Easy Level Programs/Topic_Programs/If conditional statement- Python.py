db1e279e5ce70a5b5705d3ed8805dc8aaf8c715f
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-12 11:59:48 +0530

    Create If conditional statement- Python.py

---
+++ Easy Level Programs/Topic_Programs/If conditional statement- Python.py
@@ -0,0 +1,6 @@
+def friends_in_trouble(j_angry, s_angry):
+    if(j_angry == True and s_angry == True):
+        return True
+    if(j_angry == False and s_angry == False):
+        return True
+    return False
