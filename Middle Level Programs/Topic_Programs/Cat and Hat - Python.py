84bb9e2cd35e559002e0010a2e047ef3d184c7fa
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-13 22:59:29 +0530

    Create Cat and Hat - Python.py

---
+++ Middle Level Programs/Topic_Programs/Cat and Hat - Python.py
@@ -0,0 +1,8 @@
+def cat_hat(str):
+  ##your code here##
+  ##You need to write complete code this time 
+  a = str.count("cat")
+  b = str.count("hat")
+  if(a == b):
+      return True
+  return False
