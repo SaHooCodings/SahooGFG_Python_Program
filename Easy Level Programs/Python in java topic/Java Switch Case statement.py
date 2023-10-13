deb60e942b858d73feeb9a5277588c3f6223bdab
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-14 00:21:03 +0530

    Create Java Switch Case statement.py

---
+++ Easy Level Programs/Python in java topic/Java Switch Case statement.py
@@ -0,0 +1,9 @@
+import math
+class Solution:
+    def switchCase(self, choice, arr):
+        # Code here
+        if choice == 1:
+            x = math.pi*arr[0]**2
+            return x
+        if choice == 2:
+            return arr[0]*arr[1]
