c1c4367b14afb090c95c4fdac56c1296a2cf9738
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-23 23:45:54 +0530

    Create Value equal to index value.py

---
+++ Easy Level Programs/Arrays-Searching/Value equal to index value.py
@@ -0,0 +1,9 @@
+class Solution:
+
+	def valueEqualToIndex(self,arr, n):
+		# code here
+		ans = []
+        for i in range(0, n):
+            if i+1 == arr[i]:
+                ans.append(arr[i])
+        return ans
