2259217b96264a731149c08e03b99c32250248d4
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-24 20:42:09 +0530

    Create Immediate Smaller Element.py

---
+++ Easy Level Programs/Arrays/Immediate Smaller Element.py
@@ -0,0 +1,12 @@
+class Solution:
+ 
+def immediateSmaller(self,arr,n):
+# code here
+for i in range(n-1):
+    if arr[i]>arr[i+1]:
+        arr[i]=arr[i+1]
+    else:
+        arr[i]=-1
+        
+arr[-1]=-1
+return arr
