ac85b672da0390d40baa6be967bcfea7803adfad
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 21:33:08 +0530

    Create Left View of Binary Tree.py

---
+++ Easy Level Programs/Tree/Left View of Binary Tree.py
@@ -0,0 +1,15 @@
+def leftViewUtil(result, node,level):
+    global max_level
+    if (node == None): 
+        return
+    if (max_level < level):
+        result.append(node.data)
+        max_level = level
+    leftViewUtil(result,node.left, level+1);
+    leftViewUtil(result,node.right, level+1);
+def LeftView(root):
+    global max_level
+    max_level = 0
+    result = []
+    leftViewUtil(result,root,1);
+    return result
