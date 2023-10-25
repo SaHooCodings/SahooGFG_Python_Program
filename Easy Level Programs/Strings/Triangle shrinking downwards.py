16a146bf975313408d16685a93c2afdadfdecc33
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 17:55:24 +0530

    Create Triangle shrinking downwards.py

---
+++ Easy Level Programs/Strings/Triangle shrinking downwards.py
@@ -0,0 +1,11 @@
+class Solution:
+    def triDownwards(self, S):
+        
+        ans=""
+        for i in range(len(S)):
+            for j in range(len(S)):
+                if i>j:
+                    ans+='.'   
+                else:
+                    ans+=S[j]
+        return ans
