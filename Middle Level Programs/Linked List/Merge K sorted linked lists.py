48509667d8af9f6c8c51f5d60cd4c441a58c6583
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 20:49:24 +0530

    Create Merge K sorted linked lists.py

---
+++ Middle Level Programs/Linked List/Merge K sorted linked lists.py
@@ -0,0 +1,42 @@
+class Solution:
+    #Function to merge two sorted list into one sorted list.
+    def MergeLists(self,n1,n2):
+        ret = None
+        if n2.data<n1.data:
+            ret=n2
+            n2=n2.next
+        else:
+            ret=n1
+            n1=n1.next
+        
+        tail=ret
+        
+        while n1 is not None and n2 is not None:
+            if n2.data<n1.data:
+                tail.next=n2
+                tail=n2
+                n2=n2.next
+            else:
+                tail.next=n1
+                tail=n1
+                n1=n1.next
+                
+        if n1:
+            tail.next=n1
+        if n2:
+            tail.next=n2
+        return ret
+    def mergeKLists(self,arr,K):
+        N = K
+        last = N-1
+        
+        while last != 0:
+            i = 0
+            j = last
+            while i<j:
+                arr[i] = self.MergeLists(arr[i], arr[j])
+                i+=1
+                j-=1
+                if (i >= j):
+                    last = j
+        return arr[0]
