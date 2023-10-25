cd85932ba928aa16526956a1a7d413debde8d431
Santosh sahu <110277267+SaHooCodings@users.noreply.github.com>
2023-10-25 19:15:11 +0530

    Create Identical Linked Lists.py

---
+++ Easy Level Programs/Linked List/Identical Linked Lists.py
@@ -0,0 +1,7 @@
+def areIdentical(head1, head2):
+    while head1!=None and head2!=None:
+        if head1.data!=head2.data:
+            return 0
+        head1=head1.next
+        head2=head2.next
+    return head1==None and head2==None
